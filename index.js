456cb27b38ae23d7baa2d224a8e52367f8e248ac
TQQ <1711002115@qq.com>
2019-03-20 10:34:21 +0800

    category相关路由

--- index.js
+++ index.js
@@ -3,8 +3,25 @@
  */
 const PORT = 8090;
 const express = require("express");
+//cors是通过修改响应消息头的方式允许当前客户端跨域访问
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const categoryRouter = require("./routes/admin/category")
 //启动主服务器   可以npm start  也可以 node ./index.js
 const app = express();
 app.listen(PORT,()=>{
   console.log('Server Listening '+PORT+' ...')
 })
+//使用中间件，这时可以F12查看response headers,多一行Access-Control-Allow-Origin: * 允许所有客户端
+app.use(cors())
+
+//把application/x-www-form-urlencoded类型的请求主体数据解析出来放入body属性--请求数据是k=v对  
+// app.use(bodyParser.urlencoded({
+//   extended:false
+// }))
+
+//把application/json类型的请求主体数据解析出来放入body属性--json格式解析成对象
+app.use(bodyParser.json())
+
+//挂载路由器
+app.use('/admin/category',categoryRouter)
